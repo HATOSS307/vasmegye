cfebcb130e3e47f59e9a3caa5942b0742e164712
HATOSS307 <113890316+HATOSS307@users.noreply.github.com>
2022-10-10 09:08:43 +0200

    Add files via upload

---
+++ vasmegye/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace vasmegye
+{
+    class Program
+    {
+        static void CdvEll()
+        {
+
+        }
+        
+        static void Main(string[] args)
+        {
+            string[] tablazat = File.ReadAllLines(@"C:\Users\cisco\source\repos\vasmegye\vasmegye\vas.txt");
+            string[,] tablazat2d = new string[tablazat.Length, 3];
+            int lines = tablazat.Length ;
+            for (int i = 0; i < tablazat.Length; i++)
+            {
+                string[] holder = tablazat[i].Split('-');
+                for (int j = 0; j < holder.Length; j++)
+                {
+                    tablazat2d[i, j] = holder[j];
+                    Console.WriteLine(tablazat2d[i,j]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
